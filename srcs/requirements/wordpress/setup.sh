66ccdf64e8a0979fad0ce60857fe058732eaa1d9
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-03 16:02:57 +0900

    update: wordpress setup.sh; create wp-config.php

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-wp core download --locale=ja --allow-root
+# mysql -h mariadb -P 3306 -u wordpress -ppassword
+
+if [ ! -e "wp-config.php" ] ; then
+	wp core download --allow-root
+	wp core config --dbname=wordpress \
+				   --dbuser=wordpress  \
+				   --dbpass=password \
+				   --dbhost=mariadb:3306
+	wp core install --url=mfunyu.42.fr \
+					--title=mfunyu \
+					--admin_user=mfunyu \
+					--admin_password=password \
+					--admin_email=mfunyu@example.com
+fi
 
 /usr/sbin/php-fpm7
