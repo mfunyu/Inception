81b37043f37d66d64249be005d25d9eaa836f7a8
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-23 17:42:21 +0900

    fix: add -p for makedir

--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -4,5 +4,5 @@ if ! grep "127.0.0.1	mfunyu.42.fr" /etc/hosts > /dev/null; then
 echo "127.0.0.1	mfunyu.42.fr" | sudo tee -a /etc/hosts
 fi
 
-sudo mkdir /home/mfunyu/data/server
-sudo mkdir /home/mfunyu/data/db
+sudo mkdir -p /home/mfunyu/data/server
+sudo mkdir -p /home/mfunyu/data/db
