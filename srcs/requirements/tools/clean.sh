de7f76d87c78c26d1aedade26c34c92f6ac4d061
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-23 17:39:30 +0900

    add: db volumes to docker-compose

--- srcs/requirements/tools/clean.sh
+++ srcs/requirements/tools/clean.sh
@@ -3,3 +3,5 @@
 if grep "127.0.0.1	mfunyu.42.fr" /etc/hosts > /dev/null; then
 sudo sed -i -e "/127\.0\.0\.1	mfunyu\.42\.fr/d" /etc/hosts
 fi
+
+sudo rm -Rf sudo /home/mfunyu/data
--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -5,3 +5,4 @@ echo "127.0.0.1	mfunyu.42.fr" | sudo tee -a /etc/hosts
 fi
 
 sudo mkdir /home/mfunyu/data/server
+sudo mkdir /home/mfunyu/data/db
